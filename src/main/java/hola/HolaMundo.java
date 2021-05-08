5333e9fdfdbc2919ab20be42b02fcc5663bd7035
Gregorio <martinez.gregorio@hotmail.com>
2021-05-08 12:55:40 +0200

    hotfix: aviso-emergencia

--- src/main/java/hola/HolaMundo.java
+++ src/main/java/hola/HolaMundo.java
@@ -4,5 +4,11 @@ public class HolaMundo {
 
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
+    avisoEmergenciaMaster();
   }
+
+  public static void avisoEmergenciaMaster() {
+    System.out.println("Esto es un aviso que he tenido que meter en master de emergencia");
+  }  
+  
 }
