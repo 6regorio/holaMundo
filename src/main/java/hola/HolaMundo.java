423615c1b2ee7c9ae724a13ed227d5ad4a63cd2b
Gregorio <martinez.gregorio@hotmail.com>
2021-05-08 12:52:55 +0200

    Nuevos avisos

--- src/main/java/hola/HolaMundo.java
+++ src/main/java/hola/HolaMundo.java
@@ -4,5 +4,10 @@ public class HolaMundo {
 
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
+    nuevosAvisos();
+  }
+
+  public static void nuevosAvisos() {
+    System.out.println("Esto lo he metido nuevo en la rama nuevos avisos");
   }
 }
